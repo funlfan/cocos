2aa3261fa84419e8fa39b990c1e5d3bcac5403b8
funlfan <fanlzby@icloud.com>
2023-10-11 18:03:39 +0800

    第二次提交

---
+++ Game1/index.js
@@ -0,0 +1,22 @@
+System.register("chunks:///_virtual/Game1",["./ManControl.ts"],(function(){return{setters:[null],execute:function(){}}}));
+
+System.register("chunks:///_virtual/ManControl.ts",["./rollupPluginModLoBabelHelpers.js","cc"],(function(t){var n,o,r,e;return{setters:[function(t){n=t.inheritsLoose},function(t){o=t.cclegacy,r=t._decorator,e=t.Component}],execute:function(){var c;o._RF.push({},"9d935xybmJObp2VHWL6dPqi","ManControl",void 0);var u=r.ccclass;r.property,t("ManControl",u("ManControl")(c=function(t){function o(){return t.apply(this,arguments)||this}n(o,t);var r=o.prototype;return r.start=function(){},r.update=function(t){},o}(e))||c);o._RF.pop()}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/Game1', 'chunks:///_virtual/Game1'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
